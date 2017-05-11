c5c20a69e2026857ce98af2d3fdfef81b4437d27
Triwith <triwith.m@ku.th>
2017-05-12 01:16:25 +0700

    Fix spliter bug

--- src/bookshelf/Database.java
+++ src/bookshelf/Database.java
@@ -10,10 +10,11 @@ import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
 /**
  * The Class that use for manage the database of this program.
+ * 
  * @author Triwith Mutitakul
- *
  */
 public class Database {
 	private List<Book> bookList = new ArrayList<Book>();
@@ -42,23 +43,30 @@ public class Database {
 			BufferedReader breaderForType = new BufferedReader(new InputStreamReader(inputForType));
 			while ((line = breader.readLine()) != null) {
 				temp = line.split(",");
+				//Fix , problem 
+				if(temp.length>4){
+					for(int x = 4;x<temp.length;x++){
+						temp[3]+=","+temp[x];
+					}
+				}
 				bookList.add(new Book(temp[0], temp[1], temp[2], temp[3]));
 			}
 			while ((line = breaderForType.readLine()) != null) {
 				temp = line.split(",");
-				for(String type:temp){
-					if(!typeList.contains(type)){
+				for (String type : temp) {
+					if (!typeList.contains(type)) {
 						typeList.add(type);
 					}
 				}
 			}
 			// Test Database
 			for (Book book : bookList) {
-				System.out.println(book.getName() + " -" + book.getDescription() + " -" + book.getType() + " -"
-						+ book.getLocation());
+				System.out.println(book.getName() + " -" + book.getType() + " -" + book.getLocation() + " -"
+						+ book.getDescription());
 			}
-			for(String type:typeList){
-				System.out.print(type+",");
+			// Test typeDatabase
+			for (String type : typeList) {
+				System.out.print(type + ",");
 			}
 			System.out.println();
 		} catch (Exception e) {
@@ -74,14 +82,16 @@ public class Database {
 	 * @param fileType
 	 * @param fileLocation
 	 */
-	private void add(String filename, String fileDescription, String fileType, String fileLocation) {
-		bookList.add(new Book(filename, fileDescription, fileType, fileLocation));
+	private void add(String filename, String fileType, String fileLocation, String fileDescription) {
+		bookList.add(new Book(filename, fileType, fileLocation, fileDescription));
 	}
+
 	/**
 	 * The method that use for add new type of category.
+	 * 
 	 * @param type
 	 */
-	private void addType(String type){
+	private void addType(String type) {
 		typeList.add(type);
 	}
 
@@ -94,12 +104,12 @@ public class Database {
 			output = new FileOutputStream(file);
 			outputForType = new FileOutputStream(typeFile);
 			for (int x = 0; x < bookList.size(); x++) {
-				byte[] byteTemp = (bookList.get(x).getName() + "," + bookList.get(x).getDescription() + ","
-						+ bookList.get(x).getType() + "," + bookList.get(x).getLocation() + "\n").getBytes();
+				byte[] byteTemp = (bookList.get(x).getName() + "," + bookList.get(x).getType() + ","
+						+ bookList.get(x).getLocation() + "," + bookList.get(x).getDescription() + "\n").getBytes();
 				output.write(byteTemp);
 			}
-			for(int x = 0;x<typeList.size();x++){
-				byte[] byteTypeTemp = (typeList.get(x)+",").getBytes();
+			for (int x = 0; x < typeList.size(); x++) {
+				byte[] byteTypeTemp = (typeList.get(x) + ",").getBytes();
 				outputForType.write(byteTypeTemp);
 			}
 		} catch (Exception e1) {
@@ -123,7 +133,7 @@ public class Database {
 		String type = sc.nextLine();
 		System.out.print("File Location : ");
 		String loca = sc.nextLine();
-		d.add(name, des, type, loca);
+		d.add(name, type, loca, des);
 		d.addType(type);
 		d.close();
 	}
